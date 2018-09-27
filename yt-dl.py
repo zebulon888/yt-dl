3341d6fbade92044bde8ec8d98e799b5e400939e
Ralf <e-info@luhrmann.nl>
2018-09-27 12:03:54 +0200

    Create yt-dl.py

---
+++ yt-dl.py
@@ -0,0 +1,13 @@
+import subprocess
+
+# example
+#link = ['https://youtube.com/videos/1','https://www.dailymotion.com/video/2']
+link = ['',]
+
+for i in link:
+	print('Start download ' + i + ' ...')
+	cmd = subprocess.run('youtube-dl -o %(title)s.%(ext)s ' + i,shell='False',stdout=subprocess.PIPE)
+	print('Download ' + i + ' done!\n')
+	#exit()
+
+print('done')
